72d158671f7953a3fb25ed56ff8dbbcb439012c3
Vvoltaic <twinflame265@gmail.com>
2025-04-20 03:26:31 -0400

    reformatting json -> json_files

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 """constants used by other files"""
 
-PLAYER_DATA_FOLDER_FILE_PATH = 'json/player_data/'
+PLAYER_DATA_FOLDER_FILE_PATH = 'json_files/player_data/'
 PLAYER_DATA_NAME_TEMPLATE = 'player_id_'
-PLAYER_DATA_NAME_TEMPLATE_WITH_FOLDER = 'json/player_data/player_id_'
+PLAYER_DATA_NAME_TEMPLATE_WITH_FOLDER = 'json_files/player_data/player_id_'
 
-STANDARD_BANNER_FILE_PATH = 'json/banner_data/standard_banner.json'
-LIMITED_BANNER_FILE_PATH = 'json/banner_data/limited_banner.json'
-LIGHT_CONE_FILE_PATH = 'json/banner_data/light_cone.json'
-RATE_UP_FILE_PATH = 'json/banner_data/rate_up.json'
+STANDARD_BANNER_FILE_PATH = 'json_files/banner_data/standard_banner.json'
+LIMITED_BANNER_FILE_PATH = 'json_files/banner_data/limited_banner.json'
+LIGHT_CONE_FILE_PATH = 'json_files/banner_data/light_cone.json'
+RATE_UP_FILE_PATH = 'json_files/banner_data/rate_up.json'
 
 WEB_ADDRESS = 'https://game8.co/games/Honkai-Star-Rail/archives/474951'
 SCRAPE_TARGET_HTML_NAME = 'to_be_scraped.html'
